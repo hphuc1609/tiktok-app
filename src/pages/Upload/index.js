3c43d45ad2ed7196673c8e190429886dded22dbf
PL <luuboy91@gmail.com>
2022-07-08 13:20:26 +0700

    Adding file code

---
+++ src/pages/Upload/index.js
@@ -0,0 +1,14 @@
+import Header from '~/components/Layouts/DefaultLayout/Header';
+
+function Upload() {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <h1>Upload page</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Upload;
